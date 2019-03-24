2e638dbb368b7c852fc7ece139f9fece7b15a0aa
Dhara Patel <1991dharapatel@gmail.com>
2019-03-24 16:13:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -91,7 +91,8 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  
+  for inner_name in holiday_hash[key]
     
 end
 
